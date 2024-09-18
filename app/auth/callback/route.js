fe6eed60323635b6318847e057d81fb7ddd6b54c
yzkn07 <mehmetali.1992@hotmail.com>
2024-09-18 05:45:39 +0300

    baseUrl

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -1,30 +1,31 @@
-import { NextResponse } from 'next/server'
-// The client you created from the Server-Side Auth instructions
 import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
 
 export async function GET(request) {
-  const { searchParams, origin } = new URL(request.url)
-  const code = searchParams.get('code')
-  // if "next" is in param, use it as the redirect URL
+  const { searchParams } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type') 
   const next = searchParams.get('next') ?? '/private'
 
-  if (code) {
+  if (token_hash && type) {
     const supabase = createClient()
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
+
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    })
+
     if (!error) {
-      const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
+      // Determine whether to redirect to local or production URL
       const isLocalEnv = process.env.NODE_ENV === 'development'
-      if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
-      } else {
-        return NextResponse.redirect(`${origin}${next}`)
-      }
+      const baseUrl = isLocalEnv ? 'http://localhost:3000' : 'https://nutship.com'
+      
+      // Redirect user to the correct domain with the next path
+      return redirect(`${baseUrl}${next}`)
     }
   }
 
-  // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+  // Redirect the user to an error page in case of failure
+  const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://nutship.com'
+  return redirect(`${baseUrl}/error`)
 }
