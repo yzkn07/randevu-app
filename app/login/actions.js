4689c761a66f693e70865c61f8c3554b04c233d2
yzkn07 <mehmetali.1992@hotmail.com>
2024-09-18 05:16:12 +0300

    ehh

--- app/login/actions.js
+++ app/login/actions.js
@@ -50,7 +50,7 @@ export async function signInWithGithub() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
-      redirectTo: `nutship.com/auth/callback`,
+      redirectTo: `https://www.nutship.com/auth/callback`,
     },
   })
   if (data.url) {
