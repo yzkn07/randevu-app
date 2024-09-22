15800bd637f9d6dcc2fe4f520711ec5d2f5a3b35
yzkn07 <mehmetali.1992@hotmail.com>
2024-09-22 14:55:28 +0300

    yoruldum

--- app/(user)/components/BosRandevuForm.jsx
+++ app/(user)/components/BosRandevuForm.jsx
@@ -7,8 +7,6 @@ export default function BosRandevuForm({
   subeler,
   selectedSubeId,
   setSelectedSubeId,
-  searchParams,
-  router,
 }) {
     const searchParams = useSearchParams()
     const router = useRouter()
