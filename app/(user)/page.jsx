d134cbdfe02b8a4fb19d26482df4ef381be6a954
yzkn07 <mehmetali.1992@hotmail.com>
2024-09-25 17:38:27 +0300

    buton düzenlemeleri

--- app/(user)/page.jsx
+++ app/(user)/page.jsx
@@ -72,62 +72,60 @@ export default  function Home() {
     </div>
 
       <div className="flex flex-col  p-2 border border-black bg-slate-200 text-black m-2 rounded-lg ">
-        <div className="border-b border-black p-2 bg-lime-200 rounded-t-lg text-start">
-          <p className="font-light text-xl">hızlı randevu al</p>
-        </div>
+            <div className="border-b border-black p-2 bg-lime-200 rounded-t-lg text-start">
+              <p className="font-light text-xl">hızlı randevu al</p>
+            </div>
         
-        <div className="p-2 bg-slate-300">
-          <div className="p-2">
-            <p className="font-semibold text-lg text-blue-600">şube seçin</p>
-          </div>
+                <div className="p-2 bg-slate-300">
+                        <div className="p-2">
+                          <p className="font-semibold text-lg text-blue-600">şube seçin</p>
+                        </div>
 
 
-          <div className="p-2 bg-slate-100 rounded-t-lg">
-            {subeler && 
-            <Suspense>
-              <BosRandevuForm  
-                step={step}
-                setStep={setStep}
-                subeler={subeler}
-                selectedSubeId={selectedSubeId}
-                setSelectedSubeId={setSelectedSubeId}
-                bolumler={bolumler}
-                selectedBolumId={selectedBolumId}
-                setSelectedBolumId={setSelectedBolumId}
-                doktorlar={doktorlar}
-                selectedDoktorId={selectedDoktorId}
-                setSelectedDoktorId={setSelectedDoktorId}
-                buttonIsActive={buttonIsActive}
-                setButtonIsActive={setButtonIsActive}
-              />
-            </Suspense>
-            }
-          </div>
+                        <div className="p-2 bg-slate-100 rounded-t-lg">
+                          {subeler && 
+                          <Suspense>
+                            <BosRandevuForm  
+                              step={step}
+                              setStep={setStep}
+                              subeler={subeler}
+                              selectedSubeId={selectedSubeId}
+                              setSelectedSubeId={setSelectedSubeId}
+                              bolumler={bolumler}
+                              selectedBolumId={selectedBolumId}
+                              setSelectedBolumId={setSelectedBolumId}
+                              doktorlar={doktorlar}
+                              selectedDoktorId={selectedDoktorId}
+                              setSelectedDoktorId={setSelectedDoktorId}
+                              buttonIsActive={buttonIsActive}
+                              setButtonIsActive={setButtonIsActive}
+                            />
+                          </Suspense>
+                          }
+                        </div>
 
 
-          <div className="flex justify-between p-2 rounded-b-lg border-t border-black bg-purple-300 "> 
-            {step > 0  && (
-              <button onClick={handleGeri} className="bg-red-300" type="button">geri git</button>
-            )}
-            
-            <button
-  onClick={handleDevam}
-  disabled={!buttonIsActive}
-  className={`${
-    buttonIsActive 
-      ? 'bg-blue-500 hover:bg-blue-600 text-white shadow-lg' 
-      : 'bg-gray-300 text-gray-500 cursor-not-allowed'
-  } 
-  px-6 py-3 rounded-lg transition-all duration-300 ease-in-out transform hover:scale-105 focus:outline-none disabled:opacity-50`}
-  type="button"
->
-  devam et
-</button>
+                              <div className={`h-16 w-full flex justify-between items-center relative p-2 rounded-b-lg border-t border-black bg-orange-400`}> 
+                                  {step > 0 && (
+                                    <button onClick={handleGeri} className="bg-red-300" type="button">geri git</button>
+                                  )}
+                                        <button
+                                          onClick={handleDevam}
+                                          disabled={!buttonIsActive}
+                                          className={`absolute w-fit
+                                            ${
+                                              buttonIsActive
+                                                ? "bg-blue-500 hover:bg-blue-600 text-white transform translate-x-[calc(100vw-180px)]  shadow-lg  left-0 m-2" 
+                                                : "bg-gray-300 text-gray-500 cursor-not-allowed left-0 m-2"
+                                            }
+                                            px-6 py-3 rounded-lg transition-transform duration-1000 ease-in-out hover:scale-105 focus:outline-none disabled:opacity-50`}
+                                          type="button"
+                                        >
+                                          devam et
+                                        </button>
+                              </div>
 
-           
-          </div>
-
-        </div>
+                </div>
 
             {/* seçilen bilgileri gösterme */}
               {selectedSube && 
@@ -137,8 +135,8 @@ export default  function Home() {
                   selectedDoktor={selectedDoktor}
                   />
               }
-  
-      </div>
+              
+       </div>
   </>
   );
 }
