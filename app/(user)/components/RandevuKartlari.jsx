d3430f8d695152fe6b97a66d9d5f86556d6e93ca
yzkn07 <mehmetali.1992@hotmail.com>
2024-09-30 15:06:21 +0300

    randevu kartları düzenlenicek

--- app/(user)/components/RandevuKartlari.jsx
+++ app/(user)/components/RandevuKartlari.jsx
@@ -8,15 +8,9 @@ export default function RandevuKartlari({bosRandevular, handleRandevu}) {
           className="bg-white rounded-lg shadow-lg p-4 hover:shadow-xl cursor-pointer transition-shadow duration-300"
           onClick={() => handleRandevu(randevu.id)}
         >
-          <h3 className="text-lg font-bold text-blue-600">{randevu.doktor}</h3>
           <p className="text-md text-gray-700">
-            {randevu.baslangic_zamani} - {randevu.bitis_zamani}
+            {randevu.randevu_zamani}
           </p>
-          <p className="text-sm text-gray-500">{randevu.sube}</p>
-          <p className="text-sm text-gray-500">{randevu.bolum}</p>
-          <button className="mt-4 bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition duration-300">
-            Randevu Al
-          </button>
         </div>
       ))}
     </div>
