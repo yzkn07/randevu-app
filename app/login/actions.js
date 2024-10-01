c186bc1e523b596ba386ddf11c64c5583e417e61
yzkn07 <mehmetali.1992@hotmail.com>
2024-10-01 21:19:41 +0300

    login sayfası ui güncellendi

--- app/login/actions.js
+++ app/login/actions.js
@@ -5,8 +5,8 @@ import { createClient } from '@/utils/supabase/server'
 
 export async function login(formData) {
   const supabase = createClient()
-
-  const randevuId = formData.get('randevuId') || null
+  
+  const randevuId = formData.get('randevuId') || null;
 
   const data = {
     email: formData.get('email'),
@@ -35,14 +35,16 @@ export async function login(formData) {
 
 
 export async function signup(formData) {
-  const supabase = createClient()
 
+  const supabase = createClient()
+  
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const email = formData.get('email')
   const password = formData.get('password')
   const firstName = formData.get('firstName')
   const lastName = formData.get('lastName')
+  const randevuId = formData.get('randevuId')
 
   // kullanıcı kaydı
   const { data: userData, error: userError } = await supabase.auth.signUp({
@@ -69,19 +71,25 @@ export async function signup(formData) {
   }
 
   revalidatePath('/', 'layout')
-  redirect('/login')
+
+ if (randevuId) {
+  // Eğer randevuId varsa, login formuna randevuId parametresiyle birlikte yönlendir
+  redirect(`/login?randevu-id=${randevuId}&isLogin=true`);
+} else {
+  redirect('/login');
+}
 }
 
 
-export async function signInWithGithub() {
-  const supabase = createClient()
-  const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: "github",
-    options: {
-      redirectTo: `http://localhost:3000/auth/callback`,
-    },
-  })
-  if (data.url) {
-    redirect(data.url) // use the redirect API for your server framework
-  }
-}
+// export async function signInWithGithub() {
+//   const supabase = createClient()
+//   const { data, error } = await supabase.auth.signInWithOAuth({
+//     provider: "github",
+//     options: {
+//       redirectTo: `http://localhost:3000/auth/callback`,
+//     },
+//   })
+//   if (data.url) {
+//     redirect(data.url) // use the redirect API for your server framework
+//   }
+// }
